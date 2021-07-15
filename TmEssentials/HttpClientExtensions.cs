b367f774f53dde75a43d5e211789c2081a3ba6fb
Petr Pivoňka <petrpiv1@gmail.com>
2021-07-15 20:08:28 +0200

    Add HeadAsync

---
+++ TmEssentials/HttpClientExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TmEssentials
+{
+    public static class HttpClientExtensions
+    {
+
+#if NETSTANDARD2_0
+
+        public static async Task<HttpResponseMessage> HeadAsync(this HttpClient http, string requestUri)
+        {
+            var head = new HttpRequestMessage(HttpMethod.Head, requestUri);
+            return await http.SendAsync(head);
+        }
+
+#endif
+
+    }
+}
