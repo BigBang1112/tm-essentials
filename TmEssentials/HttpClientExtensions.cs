88892d42e2abfb226728afd85cfdd9ec4398fc3d
Petr Pivoňka <petrpiv1@gmail.com>
2021-07-16 03:42:42 +0200

    Wrap namespace

--- TmEssentials/HttpClientExtensions.cs
+++ TmEssentials/HttpClientExtensions.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net;
-using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+
+#if NETSTANDARD2_0
+
+using System.Net.Http;
+
+#endif
 
 namespace TmEssentials
 {
