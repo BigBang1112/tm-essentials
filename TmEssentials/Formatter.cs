e7c40eda6122bd15d89a1d5b7828da8d799bb7c9
Petr Pivoňka <petrpiv1@gmail.com>
2021-08-04 22:25:58 +0200

    Remove brackets from deformat regex

--- TmEssentials/Formatter.cs
+++ TmEssentials/Formatter.cs
@@ -8,8 +8,9 @@ namespace TmEssentials
     public static class Formatter
     {
         private static readonly Regex deformatRegex =
-            new Regex("(\\$[wnoitsgz><]|\\$[lh]\\[.+\\]|\\$[lh]|\\$[0-9a-f]{1,3})",
+            new Regex("\\$[wnoitsgz><]|\\$[lh]\\[.+\\]|\\$[lh]|\\$[0-9a-f]{1,3}",
                 RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        // Credits to reaby for the pattern (https://github.com/reaby)
 
         public static string Deformat(string str)
         {
