bf7d640b6a47e5011a3ed06ea2de5883c9940ef6
Petr Pivoňka <petrpiv1@gmail.com>
2022-08-03 23:55:06 +0200

    Use BenchmarkSwitcher

--- TmEssentials.Benchmarks/Program.cs
+++ TmEssentials.Benchmarks/Program.cs
@@ -1,17 +1,3 @@
 ﻿using BenchmarkDotNet.Running;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace TmEssentials.Benchmarks
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            BenchmarkRunner.Run<TimeSpanExtensionsBenchmarks>();
-        }
-    }
-}
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
