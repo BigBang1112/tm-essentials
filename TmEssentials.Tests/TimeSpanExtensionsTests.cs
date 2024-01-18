99045b0aee5ac52d1367593385759283ea59a975
Petr Pivoňka <petrpiv1@gmail.com>
2024-01-18 15:59:39 +0100

    Add more tests

--- TmEssentials.Tests/TimeSpanExtensionsTests.cs
+++ TmEssentials.Tests/TimeSpanExtensionsTests.cs
@@ -24,4 +24,44 @@ public class TimeSpanExtensionsTests
 
         Assert.Equal(expected, actual);
     }
+
+    [Theory]
+    [InlineData(false, false)]
+    [InlineData(true, false)]
+    [InlineData(false, true)]
+    [InlineData(true, true)]
+    public void ToTmString_CallsToString(bool useHundredths, bool useApostrophe)
+    {
+        var expected = new TimeInt32(234567890).ToString(useHundredths, useApostrophe);
+
+        var actual = TimeSpan.FromMilliseconds(234567890).ToTmString(useHundredths, useApostrophe);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(false, false)]
+    [InlineData(true, false)]
+    [InlineData(false, true)]
+    [InlineData(true, true)]
+    public void ToTmString_CallsToString_NullableWithValue(bool useHundredths, bool useApostrophe)
+    {
+        var expected = new TimeInt32(234567890).ToString(useHundredths, useApostrophe);
+
+        var actual = new TimeSpan?(TimeSpan.FromMilliseconds(234567890)).ToTmString(useHundredths, useApostrophe);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(false, false, "-:--.---")]
+    [InlineData(true, false, "-:--.--")]
+    [InlineData(false, true, "-'--''---")]
+    [InlineData(true, true, "-'--''--")]
+    public void ToTmString_CallsToString_NullableWithoutValue(bool useHundredths, bool useApostrophe, string expected)
+    {
+        var actual = new TimeSpan?().ToTmString(useHundredths, useApostrophe);
+
+        Assert.Equal(expected, actual);
+    }
 }
