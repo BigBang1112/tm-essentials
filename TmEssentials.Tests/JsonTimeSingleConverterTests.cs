7eaa5e63cf47a76f5db0f42f7dd0b24176ff332f
Petr Pivoňka <petrpiv1@gmail.com>
2025-10-16 21:13:51 +0200

    Add tests for the json converters

---
+++ TmEssentials.Tests/JsonTimeSingleConverterTests.cs
@@ -0,0 +1,68 @@
+﻿using System.Text.Json;
+using TmEssentials.Converters;
+using Xunit;
+
+namespace TmEssentials.Tests;
+
+public sealed class JsonTimeSingleConverterTests
+{
+    private static JsonSerializerOptions CreateOptions()
+    {
+        var o = new JsonSerializerOptions();
+        o.Converters.Add(new JsonTimeSingleConverter());
+        return o;
+    }
+
+    private sealed class SampleContainer
+    {
+        public TimeSingle Time { get; set; }
+    }
+
+    [Fact]
+    public void Serialize_Root_WritesNumber()
+    {
+        var options = CreateOptions();
+        var json = JsonSerializer.Serialize(new TimeSingle(1234.5f), options);
+
+        Assert.Equal("1234.5", json);
+    }
+
+    [Fact]
+    public void Deserialize_Root_ReadsNumber()
+    {
+        var options = CreateOptions();
+        var value = JsonSerializer.Deserialize<TimeSingle>("1234.5", options);
+
+        Assert.Equal(1234.5f, value.TotalSeconds);
+    }
+
+    [Fact]
+    public void Serialize_Property_WritesNumber()
+    {
+        var options = CreateOptions();
+        var json = JsonSerializer.Serialize(new SampleContainer { Time = new TimeSingle(98765.125f) }, options);
+
+        Assert.Equal("{\"Time\":98765.125}", json);
+    }
+
+    [Fact]
+    public void Deserialize_Property_ReadsNumber()
+    {
+        var options = CreateOptions();
+        var obj = JsonSerializer.Deserialize<SampleContainer>("{\"Time\":98765.125}", options);
+
+        Assert.NotNull(obj);
+        Assert.Equal(98765.125f, obj!.Time.TotalSeconds);
+    }
+
+    [Fact]
+    public void NegativeValue_SerializesCorrectly()
+    {
+        var options = CreateOptions();
+        const float secs = -123456.75f;
+        var json = JsonSerializer.Serialize(new TimeSingle(secs), options);
+        var back = JsonSerializer.Deserialize<TimeSingle>(json, options);
+
+        Assert.Equal(secs, back.TotalSeconds);
+    }
+}
